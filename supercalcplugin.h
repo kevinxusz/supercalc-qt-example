4977c675cf02fcc17a00c181fe5cacc288cde00b
James-Adam Renquinha Henri <jarhmander@gmail.com>
2017-04-02 22:11:33 -0400

    Interface definition.

--- supercalcplugin.h
+++ supercalcplugin.h
@@ -1,4 +1,18 @@
 #ifndef SUPERCALCPLUGIN_H
 #define SUPERCALCPLUGIN_H
 
+#include <string>
+#include <memory>
+
+class SuperCalcPlugin
+{
+public:
+    virtual ~SuperCalcPlugin() {}
+
+    virtual double operation(double op1, double op2) = 0;
+    virtual std::string get_name() const = 0;
+};
+
+extern "C" std::unique_ptr<SuperCalcPlugin> SuperCalcPlugin_new();
+
 #endif // SUPERCALCPLUGIN_H
